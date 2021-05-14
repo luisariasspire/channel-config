36a53bbaa0ef999b227df2263b3abdab0aaae95b
Barry King <54867411+barry-king-spire@users.noreply.github.com>
2021-05-14 08:51:51 -0600

    Remove old fields in channel config (#17162)
    * remove old fields
    
    * add broken configs test
    
    * add line at end of file
    
    * Update lib/channel_config/test/example_data/broken_configs.yaml
    
    Co-authored-by: Nick Pascucci <82097533+nick-pascucci-spire@users.noreply.github.com>
    
    * Update lib/channel_config/test/example_data/broken_configs.yaml
    
    Co-authored-by: Nick Pascucci <82097533+nick-pascucci-spire@users.noreply.github.com>
    
    * clean up old tests
    
    Co-authored-by: Nick Pascucci <82097533+nick-pascucci-spire@users.noreply.github.com>

--- channel_tool.py
+++ channel_tool.py
@@ -669,11 +669,6 @@ def add_editing_flags(parser: Any) -> None:
         help="Whether the channel is licensed for legal use.",
     )
     parser.add_argument(
-        "--goodput_kbps",
-        type=float,
-        help="Estimated upper bound on download rate for this channel.",
-    )
-    parser.add_argument(
         "--contact_overhead_time",
         type=str,
         help=(
@@ -690,6 +685,11 @@ def add_editing_flags(parser: Any) -> None:
         "--satellite_constraints",
         type=str_to_yaml,
         help="A YAML block describing the satellite constraints.",
+    )
+    parser.add_argument(
+        "--link_profile",
+        type=str_to_yaml,
+        help="A YAML block describing the link profile of this contact type.",
     )
 
 
